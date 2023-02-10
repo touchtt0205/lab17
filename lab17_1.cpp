4d2ddebd34754e9166d529d444f5629e72ff1626
touch <tttouch1122@gmail.com>
2023-02-10 16:41:04 +0700

    Full

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,41 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double a[0],int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if((i +1)%M == 0 ) cout << endl;
+       
+        else cout << " ";
+    }
+}
+void randData(double a[],int N,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = rand()%101*0.01;
+    }
+}
+void findRowSum(const double *a,double * b,int N,int M){
+    
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N; i++){
+    double sum1;
+        for(int j = 0;j<M;j++){
+            sum1 = sum1 + *(a+j + i*M);
+        }
+       *(b+i) = sum1;
+       sum1 = 0;
+    }
+}
+void findColSum(const double *a,double * b,int N,int M){
+    
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < M; i++){
+    double sum1;
+        for(int j = 0;j<N;j++){
+            sum1 = sum1 + *(a+i + j*M);
+        }
+       *(b+i) = sum1;
+       sum1 = 0;
+    }
+}
